04a2c1e75f32ae3f5f65830c93c47d75a3d1e68d
AEfrain23 <angelefrain23@gmail.com>
2022-08-11 11:03:49 +0100

    more on models

--- application/models.py
+++ application/models.py
@@ -6,11 +6,11 @@ class Drivers(db.model):
 class Delivery(db.model):
     delivery_id = db.Column(db.Integer, primary_key = True)
     driver_id = db.Column(db.Integer, db.ForeignKey('drivers.driver_id'))
-    delivery_date = db.Column(db.) #How to do date?
+    delivery_date = db.Column(db.DateTime, nullable = False)
     delivery_packages = db.relationship('Packages', backref = 'Delivery')
 
 class Packages(db.model):
     order_id = db.Column(db.Integer, primary_key = True)
     address = db.Column(db.string(50), nullable = False)
-    staus = db.Column(db.Boolean) #How to do boolean?
-    
+    staus = db.Column(db.Boolean, ) #How to do boolean?
+
