2091aa3c3baac7ce6242001b974523916f59cf79
Sibel97 <s_hassan97@hotmail.co.uk>
2022-08-11 11:19:26 +0100

    First form commit

---
+++ application/forms.py
@@ -0,0 +1,9 @@
+from flask import Flask 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, BooleanField, IntegerField
+from wtfforms.validators import DataRequired, length, ValidationError
+from datetime import date, datetime
+
+class DriverForm(FlaskForm):
+    driver_name = StringField('Driver Name', validators=[DataRequired(), length (min=2, max = 50) ])
+    submit = SubmitField('Enter')
