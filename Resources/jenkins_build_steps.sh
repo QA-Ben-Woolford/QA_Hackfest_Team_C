a612f3c42f74c6fa28d2cc22c9289e744a76c55c
dejanmccreery <108457094+dejanmccreery@users.noreply.github.com>
2022-08-12 11:47:51 +0100

    Modified sh file uses pip3 over pip

--- Resources/jenkins_build_steps.sh
+++ Resources/jenkins_build_steps.sh
@@ -6,7 +6,7 @@ sudo apt-get install python3.10 -y
 echo "installing pip"
 sudo apt install python3-pip -y
 echo "installing requirements"
-pip install -r requirements.txt 
+pip3 install -r requirements.txt 
 echo "create database"
 python3 create.py 
 echo "run flask app"
