fb58ffb848e9fcedab451ab3f656d9f378542770
dejanmccreery <108457094+dejanmccreery@users.noreply.github.com>
2022-08-12 10:59:31 +0100

    Added sh file for jenkins

---
+++ Resources/jenkins_build_steps.sh
@@ -0,0 +1,17 @@
+chmod +x jenkins_build_steps.sh
+
+
+sudo apt update && sudo apt upgrade -y 
+sudo apt-get install python3.10 
+sudo apt install python3-pip 
+pip install -r requirements.txt 
+python3 create.py 
+python3 app.py
+
+
+ 
+
+
+
+
+
