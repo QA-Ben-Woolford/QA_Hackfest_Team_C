4ff34dc1b055e332d55217c4f3a801d846a44819
dejanmccreery <108457094+dejanmccreery@users.noreply.github.com>
2022-08-12 11:44:03 +0100

    Modified sh file again

--- Resources/jenkins_build_steps.sh
+++ Resources/jenkins_build_steps.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+echo "Update to latest dependcies"
 sudo apt update && sudo apt upgrade -y 
-sudo apt-get install python3.10 
-sudo apt install python3-pip 
+echo "installing python"
+sudo apt-get install python3.10 -y
+echo "installing pip"
+sudo apt install python3-pip -y
+echo "installing requirements"
 pip install -r requirements.txt 
+echo "create database"
 python3 create.py 
+echo "run flask app"
 python3 app.py
 EOF
+
+
+
+
+
 
 
