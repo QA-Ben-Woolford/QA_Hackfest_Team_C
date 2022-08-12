8afda51cdbf902d2f8fbf351d40c1a3ecd2010df
dejanmccreery <108457094+dejanmccreery@users.noreply.github.com>
2022-08-12 11:22:27 +0100

    Modified sh file

--- Resources/jenkins_build_steps.sh
+++ Resources/jenkins_build_steps.sh
@@ -1,17 +1,10 @@
-chmod +x jenkins_build_steps.sh
-
-
+#!/bin/bash
 sudo apt update && sudo apt upgrade -y 
 sudo apt-get install python3.10 
 sudo apt install python3-pip 
 pip install -r requirements.txt 
 python3 create.py 
 python3 app.py
-
-
- 
-
-
-
+EOF
 
 
